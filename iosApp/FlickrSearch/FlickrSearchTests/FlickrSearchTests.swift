fea3ea5f723ea0d36e17392c7f8af7b3a6833a9b
Niek Akerboom <mail@niek.dev>
2025-03-11 18:37:31 +0100

    Add iOS app to the project (generated through Xcode)

---
+++ iosApp/FlickrSearch/FlickrSearchTests/FlickrSearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrSearchTests.swift
+//  FlickrSearchTests
+//
+//  Created by Niek Akerboom on 11.03.25.
+//
+
+import Testing
+@testable import FlickrSearch
+
+struct FlickrSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
