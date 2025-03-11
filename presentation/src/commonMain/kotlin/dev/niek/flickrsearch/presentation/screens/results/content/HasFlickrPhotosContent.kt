908e15a20ea45b64bdd0fb8bab223785796d35e1
Niek Akerboom <mail@niek.dev>
2025-03-11 12:11:32 +0100

    Add a background over the Flickr photo which gets shown while loading

--- presentation/src/commonMain/kotlin/dev/niek/flickrsearch/presentation/screens/results/content/HasFlickrPhotosContent.kt
+++ presentation/src/commonMain/kotlin/dev/niek/flickrsearch/presentation/screens/results/content/HasFlickrPhotosContent.kt
@@ -1,10 +1,12 @@
 package dev.niek.flickrsearch.presentation.screens.results.content
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,7 +29,8 @@ fun HasFlickrPhotosContent(
                 imageModel = { imageUrl },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .aspectRatio(4f / 3f),
+                    .aspectRatio(4f / 3f)
+                    .background(MaterialTheme.colorScheme.surfaceContainer),
                 imageOptions = ImageOptions(
                     contentScale = ContentScale.Crop,
                     alignment = Alignment.Center,
