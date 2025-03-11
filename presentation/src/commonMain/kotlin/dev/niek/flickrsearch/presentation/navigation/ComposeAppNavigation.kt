6dc4506dce04790aba2e9c19f480382d24124399
Niek Akerboom <mail@niek.dev>
2025-03-11 20:29:44 +0100

    Introduce nice dark and light Material themes for our app

--- presentation/src/commonMain/kotlin/dev/niek/flickrsearch/presentation/navigation/ComposeAppNavigation.kt
+++ presentation/src/commonMain/kotlin/dev/niek/flickrsearch/presentation/navigation/ComposeAppNavigation.kt
@@ -7,6 +7,7 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import dev.niek.flickrsearch.presentation.screens.main.MainScreen
 import dev.niek.flickrsearch.presentation.screens.results.ResultsScreen
+import dev.niek.flickrsearch.presentation.theme.FlickrSearchTheme
 import org.koin.compose.KoinContext
 
 @Composable
@@ -14,19 +15,21 @@ fun ComposeAppNavigation() {
     val navController = rememberNavController()
 
     KoinContext {
-        NavHost(
-            navController = navController,
-            startDestination = AppRoute.MainScreenRoute.Search,
-        ) {
-            composable<AppRoute.MainScreenRoute.Search> {
-                MainScreen(navController, AppRoute.MainScreenRoute.Search)
-            }
-            composable<AppRoute.MainScreenRoute.History> {
-                MainScreen(navController, AppRoute.MainScreenRoute.History)
-            }
-            composable<AppRoute.Results> { backStackEntry ->
-                val results: AppRoute.Results = backStackEntry.toRoute()
-                ResultsScreen(navController, searchTerm = results.searchTerm)
+        FlickrSearchTheme {
+            NavHost(
+                navController = navController,
+                startDestination = AppRoute.MainScreenRoute.Search,
+            ) {
+                composable<AppRoute.MainScreenRoute.Search> {
+                    MainScreen(navController, AppRoute.MainScreenRoute.Search)
+                }
+                composable<AppRoute.MainScreenRoute.History> {
+                    MainScreen(navController, AppRoute.MainScreenRoute.History)
+                }
+                composable<AppRoute.Results> { backStackEntry ->
+                    val results: AppRoute.Results = backStackEntry.toRoute()
+                    ResultsScreen(navController, searchTerm = results.searchTerm)
+                }
             }
         }
     }
---
+++ presentation/src/commonMain/kotlin/dev/niek/flickrsearch/presentation/theme/FlickrSearchTheme.kt
@@ -0,0 +1,75 @@
+package dev.niek.flickrsearch.presentation.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColorScheme = lightColorScheme(
+    primary = md_theme_light_primary,
+    onPrimary = md_theme_light_onPrimary,
+    primaryContainer = md_theme_light_primaryContainer,
+    onPrimaryContainer = md_theme_light_onPrimaryContainer,
+    error = md_theme_light_error,
+    errorContainer = md_theme_light_errorContainer,
+    onError = md_theme_light_onError,
+    onErrorContainer = md_theme_light_onErrorContainer,
+    background = md_theme_light_background,
+    onBackground = md_theme_light_onBackground,
+    surface = md_theme_light_surface,
+    onSurface = md_theme_light_onSurface,
+    surfaceVariant = md_theme_light_surfaceVariant,
+    onSurfaceVariant = md_theme_light_onSurfaceVariant,
+    outline = md_theme_light_outline,
+    inverseOnSurface = md_theme_light_inverseOnSurface,
+    inverseSurface = md_theme_light_inverseSurface,
+    inversePrimary = md_theme_light_inversePrimary,
+    surfaceTint = md_theme_light_surfaceTint,
+    outlineVariant = md_theme_light_outlineVariant,
+    scrim = md_theme_light_scrim,
+)
+
+private val DarkColorScheme = darkColorScheme(
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    primaryContainer = md_theme_dark_primaryContainer,
+    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
+    error = md_theme_dark_error,
+    errorContainer = md_theme_dark_errorContainer,
+    onError = md_theme_dark_onError,
+    onErrorContainer = md_theme_dark_onErrorContainer,
+    background = md_theme_dark_background,
+    onBackground = md_theme_dark_onBackground,
+    surface = md_theme_dark_surface,
+    onSurface = md_theme_dark_onSurface,
+    surfaceVariant = md_theme_dark_surfaceVariant,
+    onSurfaceVariant = md_theme_dark_onSurfaceVariant,
+    outline = md_theme_dark_outline,
+    inverseOnSurface = md_theme_dark_inverseOnSurface,
+    inverseSurface = md_theme_dark_inverseSurface,
+    inversePrimary = md_theme_dark_inversePrimary,
+    surfaceTint = md_theme_dark_surfaceTint,
+    outlineVariant = md_theme_dark_outlineVariant,
+    scrim = md_theme_dark_scrim,
+)
+
+@Composable
+private fun colorScheme(
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
+) = when {
+    // normal dark theme
+    useDarkTheme -> DarkColorScheme
+    // normal light theme
+    else -> LightColorScheme
+}
+
+@Composable
+fun FlickrSearchTheme(
+    content: @Composable () -> Unit,
+) {
+    MaterialTheme(
+        colorScheme = colorScheme(),
+        content = content,
+    )
+}
