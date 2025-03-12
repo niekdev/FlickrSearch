d0faa8e22121bf67f38bbcad0f69cc8654a95ee9
Niek Akerboom <mail@niek.dev>
2025-03-12 20:14:19 +0100

    Disable transition animations in 'ComposeAppNavigation'

--- presentation/src/commonMain/kotlin/dev/niek/flickrsearch/presentation/navigation/ComposeAppNavigation.kt
+++ presentation/src/commonMain/kotlin/dev/niek/flickrsearch/presentation/navigation/ComposeAppNavigation.kt
@@ -1,5 +1,9 @@
 package dev.niek.flickrsearch.presentation.navigation
 
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -10,25 +14,32 @@ import dev.niek.flickrsearch.presentation.screens.results.ResultsScreen
 import dev.niek.flickrsearch.presentation.theme.FlickrSearchTheme
 import org.koin.compose.KoinContext
 
+@OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
 fun ComposeAppNavigation() {
     val navController = rememberNavController()
 
     KoinContext {
         FlickrSearchTheme {
-            NavHost(
-                navController = navController,
-                startDestination = AppRoute.MainScreenRoute.Search,
-            ) {
-                composable<AppRoute.MainScreenRoute.Search> {
-                    MainScreen(navController, AppRoute.MainScreenRoute.Search)
-                }
-                composable<AppRoute.MainScreenRoute.History> {
-                    MainScreen(navController, AppRoute.MainScreenRoute.History)
-                }
-                composable<AppRoute.Results> { backStackEntry ->
-                    val results: AppRoute.Results = backStackEntry.toRoute()
-                    ResultsScreen(navController, searchTerm = results.searchTerm)
+            SharedTransitionLayout {
+                NavHost(
+                    navController = navController,
+                    startDestination = AppRoute.MainScreenRoute.Search,
+                    enterTransition = { EnterTransition.None },
+                    exitTransition = { ExitTransition.None },
+                    popEnterTransition = { EnterTransition.None },
+                    popExitTransition = { ExitTransition.None },
+                ) {
+                    composable<AppRoute.MainScreenRoute.Search> {
+                        MainScreen(navController, AppRoute.MainScreenRoute.Search)
+                    }
+                    composable<AppRoute.MainScreenRoute.History> {
+                        MainScreen(navController, AppRoute.MainScreenRoute.History)
+                    }
+                    composable<AppRoute.Results> { backStackEntry ->
+                        val results: AppRoute.Results = backStackEntry.toRoute()
+                        ResultsScreen(navController, searchTerm = results.searchTerm)
+                    }
                 }
             }
         }
