8701aa7d760092463f403ca8cc1ff842952b4a6d
Niek Akerboom <mail@niek.dev>
2025-04-13 12:38:27 +0200

    Add documentation to the repository delegates

--- domain/src/commonMain/kotlin/dev/niek/flickrsearch/domain/repositories/FlickrRepository.kt
+++ domain/src/commonMain/kotlin/dev/niek/flickrsearch/domain/repositories/FlickrRepository.kt
@@ -4,7 +4,17 @@ import app.cash.paging.PagingData
 import dev.niek.flickrsearch.domain.models.FlickrImage
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Repository interface for searching photos using Flickr's API.
+ */
 interface FlickrRepository {
 
+    /**
+     * Searches for photos on Flickr based on a given search term.
+     *
+     * @param searchTerm The term or keyword used to perform the photo search.
+     * @return A [Flow] emitting [PagingData] of [FlickrImage] objects, where each object
+     * represents a photo retrieved from Flickr's API.
+     */
     fun searchPhotos(searchTerm: String): Flow<PagingData<FlickrImage>>
 }
--- domain/src/commonMain/kotlin/dev/niek/flickrsearch/domain/repositories/SearchHistoryRepository.kt
+++ domain/src/commonMain/kotlin/dev/niek/flickrsearch/domain/repositories/SearchHistoryRepository.kt
@@ -3,13 +3,35 @@ package dev.niek.flickrsearch.domain.repositories
 import dev.niek.flickrsearch.domain.models.SearchEntry
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Repository interface for managing local search history.
+ */
 interface SearchHistoryRepository {
 
+    /**
+     * Retrieves the user's search history as a stream of data.
+     *
+     * @return A [Flow] emitting a list of [SearchEntry] objects representing the search history.
+     */
     fun getSearchHistory(): Flow<List<SearchEntry>>
 
+    /**
+     * Inserts a new search query into the search history.
+     *
+     * @param searchQuery The search term to be added to the history.
+     */
     suspend fun insertSearchEntry(searchQuery: String)
 
+    /**
+     * Clears all entries from the search history.
+     */
     suspend fun clearSearchHistory()
 
+    /**
+     * Checks if there are any entries in the search history.
+     *
+     * @return A [Flow] emitting `true` if there is at least one entry in the search history, or
+     * `false` if it is empty.
+     */
     suspend fun hasSearchHistory(): Flow<Boolean>
 }
